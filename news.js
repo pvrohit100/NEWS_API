7686b571075370b3edef966cd1cb470534f1c770
pvrohit100 <56212156+pvrohit100@users.noreply.github.com>
2020-11-14 11:46:30 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,26 +1,4 @@
 
-var newsAPI = "https://gnews.io/api/v4/search?q=tech&lang=en&token=602e986d0a64f01902a96d81d70fc3de";
-        $.getJSON(newsAPI ,function (data) {
-          //#################
-          n=Math.floor((Math.random() * 7));
-
-$(one).append(data.articles[n+0].title);
-$(two).append(data.articles[n+1].title);
-$(three).append(data.articles[n+2].title);
-
-
-$(oned).append(data.articles[n+0].description);
-$(twod).append(data.articles[n+1].description);
-$(threed).append(data.articles[n+2].description);
-
-document.getElementById("onei").src = data.articles[n+0].image;
-document.getElementById("twoi").src = data.articles[n+1].image;
-document.getElementById("threei").src = data.articles[n+2].image;
-
-
-document.getElementById("oneu").href=data.articles[n+0].url;
-document.getElementById("twou").href=data.articles[n+1].url;
-document.getElementById("threeu").href=data.articles[n+2].url;
 $(document).ready(function () {
 
 
@@ -153,6 +131,28 @@ document.getElementById("threeu").href=data.articles[n+2].url;
 //////////////
   
 
+var newsAPI = "https://gnews.io/api/v4/search?q=tech&lang=en&token=602e986d0a64f01902a96d81d70fc3de";
+        $.getJSON(newsAPI ,function (data) {
+          //#################
+          n=Math.floor((Math.random() * 7));
+
+$(one).append(data.articles[n+0].title);
+$(two).append(data.articles[n+1].title);
+$(three).append(data.articles[n+2].title);
+
+
+$(oned).append(data.articles[n+0].description);
+$(twod).append(data.articles[n+1].description);
+$(threed).append(data.articles[n+2].description);
+
+document.getElementById("onei").src = data.articles[n+0].image;
+document.getElementById("twoi").src = data.articles[n+1].image;
+document.getElementById("threei").src = data.articles[n+2].image;
+
+
+document.getElementById("oneu").href=data.articles[n+0].url;
+document.getElementById("twou").href=data.articles[n+1].url;
+document.getElementById("threeu").href=data.articles[n+2].url;
 
 
 
